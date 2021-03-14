54d02703171319980c41a284e53aa937f6232e35
Никитин Илья <79610518+DarknessSpaun@users.noreply.github.com>
2021-03-15 02:10:58 +0300

    Add files via upload

---
+++ Переменные.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class Variables
+    {
+        public static double Test1(double a, double b)
+        {
+            //Пользователь вводит 2 числа(A и B).Выведите в консоль решение(5 * A + B ^ 2) / (B - A)
+            double result = (5 * a + b * b) / (b - a);
+            return result;
+        }
+
+        public static void Test2(ref int a, ref int b)
+        {
+            //Пользователь вводит 2 значения(A и B). Поменяйте содержимое переменных A и B местами.
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static int[] Test3(int a, int b)
+        {
+            //Пользователь вводит 2 числа (A и B). Выведите в консоль результат деления A на B и остаток от деления.
+            if (b == 0)
+            {
+                throw new DivideByZeroException("b == 0");
+            }
+            int result = a / b;
+            int ostatok = a % b;
+            return new int[] { result, ostatok };
+        }
+
+        public static double Test4(double a, double b, double c)
+        {
+            //Пользователь вводит 3 числа (A, B и С). Выведите в консоль решение(значение X) линейного уравнения стандартного вида, где A*X+B=C.
+            if (a == 0)
+            {
+                throw new DivideByZeroException("a==0");
+            }
+            double x = (c - b) / a;
+            return x;
+        }
+
+        public static string Test5(double x1, double x2, double y1, double y2)
+        {
+            //Пользователь вводит 4 числа (X1, Y1, X2, Y2), описывающие координаты 2-х точек на координатной плоскости. Выведите уравнение прямой в формате Y=AX+B, проходящей через эти точки.
+            double a = (y1 - y2) / (x1 - x2);
+            double b = y2 - a * x2;
+            return new string($"y= {a}x + {b}");
+        }
+    }
+}
---
+++ Циклы.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class Cycles
+    {
+        public static decimal Test1(decimal a, decimal b)
+        {
+            //Пользователь вводит 2 числа (A и B). Возвести число A в степень B.
+            decimal result = 0;
+            if (b == 0)
+            {
+                result = 1;
+                return result;
+            }
+            if (b < 0)
+            {
+                result = a;
+                for (int i = 1; i < Math.Abs(b); i++)
+                {
+                    result *= a;
+                }
+                return 1 / result;
+            }
+            result = a;
+            for (int i = 1; i < b; i++)
+            {
+                result *= a;
+            }
+            return result;
+        }
+
+        public static int[] Test2(int a)
+        {
+            //Пользователь вводит 1 число (A). Вывести все числа от 1 до 1000, которые делятся на A.
+            if (a == 0)
+            {
+                throw new DivideByZeroException("a == 0");
+            }
+            int length = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % a == 0)
+                {
+                    length++;
+                }
+            }
+            int[] result = new int[length];
+            int n = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % a == 0)
+                {
+                    result[n] = i;
+                    n++;
+                }
+            }
+            return result;
+        }
+
+        public static int Test3(double a)
+        {
+            //Пользователь вводит 1 число(A).Найдите количество положительных целых чисел, квадрат которых меньше A.
+            if (a <= 0)
+            {
+                throw new ArgumentException("a <= 0");
+            }
+            int result = 0;
+            double sqrt = Math.Sqrt(a);
+            for (double i = 1; i < sqrt; i++)
+            {
+                result++;
+            }
+            return result;
+        }
+        public static int Test4(int a)
+        {
+            //Пользователь вводит 1 число (A). Вывести наибольший делитель (кроме самого A) числа A.
+            if (a == 0)
+            {
+                throw new ArgumentException("a == 0");
+            }
+            int i = Math.Abs(a);
+            do
+            {
+                i--;
+            } while (a % i != 0);
+            if(a < 0)
+            {
+                return -1 * i;
+            }
+            return i;
+        }
+
+        public static int Test5(int a, int b)
+        {
+            //Пользователь вводит 2 числа (A и B). Вывести сумму всех чисел из диапазона от A до B, которые делятся без остатка на 7. (Учтите, что при вводе B может оказаться меньше A).
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("a == 0 и b == 0");
+            }
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+            int result = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+
+        public static int Test6(int n)
+        {
+            //Пользователь вводит 1 число (N). Выведите N-ое число ряда фибоначчи. В ряду фибоначчи каждое следующее число является суммой двух предыдущих. Первое и второе считаются равными 1.
+            if (n == 0)
+            {
+                throw new ArgumentException("n == 0");
+            }
+            int result = 0;
+            if (n < 1)
+            {
+                throw new ArgumentException("n < 1");
+            }
+            else
+            {
+                int first = 1;
+                int second = 1;
+                
+
+                int i = 2;
+                while (i < n)
+                {
+                    result = first + second;
+                    first = second;
+                    second = result;
+                    i++;
+                }
+            }
+            return result;
+        }
+        public static int Test7(int a, int b)
+        {
+            //Пользователь вводит 2 числа. Найти их наибольший общий делитель используя алгоритм Евклида.
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("a == 0 и b == 0");
+            }
+            while (b != 0)
+            {
+                b = a % (a = b);
+            }
+            return(Math.Abs(a));
+        }
+
+        public static int Test8(double n)
+        {
+            //Пользователь вводит целое положительное число, которое является кубом целого числа N. Найдите число N методом половинного деления.
+            if (n < 0)
+            {
+                throw new ArgumentException("n < 0");
+            }
+            if (n == 0)
+            {
+                throw new ArgumentException("n == 0");
+            }
+            double left = 0d;
+            double right = n;
+            double middle = right;
+            double delta = 0.01d;
+            while (!(n <= Math.Pow((middle + delta),3) && n >= Math.Pow((middle - delta),3)))
+            {
+                if (middle * middle * middle > n)
+                {
+                    right = middle;
+                }
+                else
+                {
+                    left = middle;
+                }
+                middle = (left + right) / 2;
+            }
+            return Convert.ToInt32(middle);
+        }
+
+        public static int Test9(int a)
+        {
+            //Пользователь вводит 1 число. Найти количество нечетных цифр этого числа.
+            int summ = 0;
+            while (a != 0)
+            {
+                if (a % 2 != 0)
+                {
+                    summ += 1;
+                }
+                a /= 10;
+            }
+            return summ;
+        }
+        public static int Test10(int a)
+        {
+            //Пользователь вводит 1 число. Найти число, которое является зеркальным отображением последовательности цифр заданного числа, например, задано число 123, вывести 321.
+            int result = 0;
+            while (a != 0)
+            {
+                if (result != 0)
+                {
+                    result *= 10;
+                }
+                int i = a % 10;
+                result += i;
+                a /= 10;
+            }
+            return result;
+        }
+        public static int[] Test11(int n)
+        {
+            //Пользователь вводит целое положительное  число (N). Выведите числа в диапазоне от 1 до N, сумма четных цифр которых больше суммы нечетных. 
+            if (n < 0)
+            {
+                throw new ArgumentException("n < 0");
+            }
+            if (n == 0)
+            {
+                throw new ArgumentException("n == 0");
+            }
+            int length = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int number = i;
+                int chtn = 0;
+                int nchtn = 0;
+                while (number > 0)
+                {
+                    int temp = number % 10;
+                    if (temp % 2 == 0)
+                    {
+                        chtn += temp;
+                    }
+                    else
+                    {
+                        nchtn += temp;
+                    }
+                    number /= 10;
+                }
+                if (chtn > nchtn)
+                {
+                    length = length + 1;
+                }
+            }
+            int[] res = new int[length];
+            int l = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int number = i;
+                int chtn = 0;
+                int nchtn = 0;
+                while (number > 0)
+                {
+                    int temp = number % 10;
+                    if (temp % 2 == 0)
+                    {
+                        chtn += temp;
+                    }
+                    else
+                    {
+                        nchtn += temp;
+                    }
+                    number /= 10;
+                }
+                if (chtn > nchtn)
+                {
+                    res[l] = i;
+                    l++;
+                }      
+            }
+            return res;
+        }
+        public static string Test12(int a, int b)
+        {
+            //Пользователь вводит 2 числа.Сообщите, есть ли в написании двух чисел одинаковые цифры. Например, для пары 123 и 3456789, ответом будет являться “ДА”, а, для пары 500 и 99 - “НЕТ”.
+            int num1 = Math.Abs(a);
+            int num2 = Math.Abs(b);
+            bool same = false;
+            while (num1 > 0)
+            {
+                int temp1 = num1 % 10;
+                while (num2 > 0)
+                {
+                    int temp2 = num2 % 10;
+                    same = (temp1 == temp2);
+                    num2 /= 10;
+                }
+                num1 /= 10;
+            }
+            if(same == false)
+            {
+                return new string("Нет");
+            }
+            return new string("Да");
+        }
+    }
+}
